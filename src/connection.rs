661c9c2fec896836ad70f1e24b5d66a2207ff9e3
Connor Kuehl <cipkuehl@gmail.com>
2020-09-21 13:37:42 -0500

    First stab at put

--- src/connection.rs
+++ src/connection.rs
@@ -47,7 +47,33 @@ impl Connection {
         Ok(writer)
     }
 
-    pub fn put<R: Read>(&self, reader: R) -> Result<()> {
-        unimplemented!()
+    pub fn put<R: Read>(&self, mut reader: R) -> Result<()> {
+        let mut current_block = 1;
+
+        loop {
+            let mut buf = [0; MAX_PAYLOAD_SIZE];
+
+            let bytes_read = reader.read(&mut buf)?;
+            let data = Packet::data(Block::new(current_block), buf[..bytes_read].to_vec());
+
+            let _ = self.socket.send(&mut data.into_bytes()[..])?;
+
+            let mut buf = [0; MAX_PACKET_SIZE];
+            let bytes_recvd = self.socket.recv(&mut buf)?;
+
+            let ack = match Packet::<Ack>::from_bytes(&buf[..bytes_recvd]) {
+                Err(_) => {
+                    let error = Packet::<Error>::from_bytes(&buf[..])?;
+                    /* FIXME */
+                    return Err(io::Error::new(io::ErrorKind::Other, "got error packet"));
+                },
+                Ok(a) => a,
+            };
+
+            assert_eq!(Block::new(current_block), ack.body.block);
+            current_block += 1;
+        }
+
+        Ok(())
     }
 }
