0afe074fe9ae40bb11c18bfdff210946bc5eec09
Connor Kuehl <cipkuehl@gmail.com>
2020-09-21 08:58:01 -0500

    impl Packet for Packet

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -1,7 +1,9 @@
 use std::convert::AsRef;
-use std::io::{self, Result};
+use std::io::{self, ErrorKind, Result};
+use std::mem::size_of;
 
 use crate::bytes::{Bytes, FromBytes, IntoBytes};
+use opcode::Opcode;
 
 mod ack;
 mod data;
@@ -12,7 +14,7 @@ mod rq;
 
 mod sealed {
     use crate::bytes::{FromBytes, IntoBytes};
-    use super::opcode::Opcode;
+    use crate::packet::opcode::Opcode;
 
     pub trait Packet: FromBytes + IntoBytes {
         const OPCODE: Opcode;
@@ -37,5 +39,38 @@ impl IntoBytes for Block {
     fn into_bytes(self) -> Vec<u8> {
         let bytes = Bytes::new(self.0);
         bytes.into_bytes()
+    }
+}
+
+pub struct Packet<T: sealed::Packet> {
+    header: Opcode,
+    body: T,
+}
+
+impl<T: sealed::Packet> FromBytes for Packet<T> {
+    type Error = io::Error;
+
+    fn from_bytes<B: AsRef<[u8]>>(bytes: B) -> Result<Self> {
+        let bytes = bytes.as_ref();
+        let (header, body) = bytes.split_at(size_of::<u16>());
+        let opcode = Opcode::from_bytes(header)?;
+        if opcode != T::OPCODE {
+            return Err(ErrorKind::InvalidData.into());
+        }
+
+        /* FIXME: Remove map_err and just use `?` */
+        let body = T::from_bytes(body)
+            .map_err(|_| -> io::Error { ErrorKind::InvalidData.into() })?;
+
+        Ok(Self { header: opcode, body })
+    }
+}
+
+impl<T: sealed::Packet> IntoBytes for Packet<T> {
+    fn into_bytes(self) -> Vec<u8> {
+        let mut body = self.body.into_bytes();
+        let mut bytes = self.header.into_bytes();
+        bytes.append(&mut body);
+        bytes
     }
 }
