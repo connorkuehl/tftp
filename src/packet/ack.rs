e0812c5bb9a75c16cb598d960b860810e05aec35
Connor Kuehl <cipkuehl@gmail.com>
2020-09-21 08:31:24 -0500

    impl Packet for Ack

--- src/packet/ack.rs
+++ src/packet/ack.rs
@@ -1,9 +1,33 @@
+use std::io::{self, Result};
+use std::mem::size_of;
+
+use crate::bytes::{FromBytes, IntoBytes};
+use crate::packet::sealed::Packet;
+use crate::packet::opcode::Opcode;
 use super::Block;
 
-/// An `Ack` packet acknowledges successful receipt of a `Data` packet
-/// and indicates that the next `Data` packet should be sent.
-#[derive(Debug)]
 pub struct Ack {
-    /// The block identifier that is being acknowledged.
     pub block: Block,
+}
+
+impl Packet for Ack {
+    const OPCODE: Opcode = Opcode::Ack;
+}
+
+impl FromBytes for Ack {
+    type Error = io::Error;
+
+    fn from_bytes<T: AsRef<[u8]>>(bytes: T) -> Result<Self> {
+        let bytes = bytes.as_ref();
+        let block = &bytes[..size_of::<Block>()];
+        let block = Block::from_bytes(block)?;
+
+        Ok(Self { block })
+    }
+}
+
+impl IntoBytes for Ack {
+    fn into_bytes(self) -> Vec<u8> {
+        self.block.into_bytes()
+    }
 }
