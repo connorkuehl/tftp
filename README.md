10388ec4118f1a2008290ee25c4b11879851415d
Connor Kuehl <cipkuehl@gmail.com>
2020-09-21 18:16:40 -0500

    Add README generated with cargo-readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# tftp
+
+The `tftp` crate provides implementations for the following components of
+the Trivial File Transfer Protocol (RFC 1350):
+
+* The protocol (types that represent TFTP packets as well as types that
+  can participate in the TFTP flow for reading or writing files with
+  TFTP.
+* A client
+* A server
+
+For more information, please see [THE TFTP PROTOCOL (REVISION 2)](
+https://tools.ietf.org/html/rfc1350).
+
+License: Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! The `tftp` crate provides implementations for the following components of
+//! the Trivial File Transfer Protocol (RFC 1350):
+//!
+//! * The protocol (types that represent TFTP packets as well as types that
+//!   can participate in the TFTP flow for reading or writing files with
+//!   TFTP.
+//! * A client
+//! * A server
+//!
+//! For more information, please see [THE TFTP PROTOCOL (REVISION 2)](
+//! https://tools.ietf.org/html/rfc1350).
+
 mod bytes;
 mod client;
 mod connection;
