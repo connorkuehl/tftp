f98c5fd8331689dc2ae33b7c96de5bdee41bd250
Connor Kuehl <cipkuehl@gmail.com>
2020-09-21 08:27:41 -0500

    impl Packet for Data

--- src/packet/data.rs
+++ src/packet/data.rs
@@ -1,12 +1,39 @@
+use std::io::{self, Result};
+use std::mem::size_of;
+
+use crate::bytes::{FromBytes, IntoBytes};
+use crate::packet::sealed::Packet;
+use crate::packet::opcode::Opcode;
 use super::Block;
 
-/// `Data` is a packet that contains a 2-byte block number and up to 512
-/// bytes of data.
-#[derive(Debug)]
 pub struct Data {
-    /// The block identifier for this data.
     pub block: Block,
-
-    /// The payload.
     pub data: Vec<u8>,
+}
+
+impl Packet for Data {
+    const OPCODE: Opcode = Opcode::Data;
+}
+
+impl FromBytes for Data {
+    type Error = io::Error;
+
+    fn from_bytes<T: AsRef<[u8]>>(bytes: T) -> Result<Self> {
+        let bytes = bytes.as_ref();
+        let (block, data) = bytes.split_at(size_of::<Block>());
+        let block = Block::from_bytes(block)?;
+        let data = data.to_vec();
+
+        Ok(Self { block, data })
+    }
+}
+
+impl IntoBytes for Data {
+    fn into_bytes(self) -> Vec<u8> {
+        let block = self.block.into_bytes();
+        let mut data = self.data;
+        let mut bytes = block;
+        bytes.append(&mut data);
+        bytes
+    }
 }
