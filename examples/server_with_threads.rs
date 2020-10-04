d33042bec03687785fb96c00e10ea49f5cae8694
Antonino Bertulla <antonino.bertulla@beefree.io>
2020-10-04 11:36:07 +0200

    Added server example with threads

---
+++ examples/server_with_threads.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::thread;
+
+use tftp::Server;
+
+fn main() {
+    let mut args = env::args().skip(1);
+    let addr = args.next().unwrap();
+    let wd = args.next().unwrap();
+
+    let server = Server::new(addr.clone(), wd).unwrap();
+    println!("Serving Trivial File Transfer Protocol (TFTP) @ {}", addr);
+
+    while let Ok(h) = server.serve() {
+        print!("Handling request...");
+
+        thread::spawn(|| match h.handle() {
+            Ok(()) => println!("OK"),
+            Err(e) => println!("FAIL: {:?}", e),
+        });
+    }
+}
